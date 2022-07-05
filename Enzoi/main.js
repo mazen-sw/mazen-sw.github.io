0dd6e9803e00b8b36bad09a921d15afdeab1c425
mazen-sw <mazenx1234@gmail.com>
2022-07-05 06:33:55 +0200

    ret

--- Enzoi/main.js
+++ Enzoi/main.js
@@ -1,7 +1,22 @@
-function LightenDarkenColor(col, amt) {
-    col = parseInt(col, 16);
-    return (((col & 0x0000FF) + amt) | ((((col >> 8) & 0x00FF) + amt) << 8) | (((col >> 16) + amt) << 16)).toString(16);
-  }
+function ColorLuminance(hex, lum) {
+
+	// validate hex string
+	hex = String(hex).replace(/[^0-9a-f]/gi, '');
+	if (hex.length < 6) {
+		hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+	}
+	lum = lum || 0;
+
+	// convert to decimal and change luminosity
+	var rgb = "#", c, i;
+	for (i = 0; i < 3; i++) {
+		c = parseInt(hex.substr(i*2,2), 16);
+		c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+		rgb += ("00"+c).substr(c.length);
+	}
+
+	return rgb;
+}
 let head = document.getElementById("head")
 let foot = document.getElementById("footer")
 foot.style.paddingRight = (window.getComputedStyle(head).width)
@@ -9,6 +24,35 @@ let changecolor = document.getElementById("changecolor")
 changecolor.setAttribute("value",getComputedStyle(document.documentElement).getPropertyValue('--idncolor').substring(1))
 function idnchange(){
     document.documentElement.style.setProperty("--idncolor",changecolor.value)
+    document.documentElement.style.setProperty("--lightidn",ColorLuminance(changecolor.value, .3))
     console.log("dn");
-    changecolor.value = LightenDarkenColor(changecolor.value,40)
+    
+    //ColorLuminance(changecolor.value, .9)
+}
+
+function contact(){
+    
+}
+let signbtn = document.getElementById("signup")
+let ppup = document.getElementById("ppup")
+function appear() {
+    ppup.style.display = "block"
+}
+
+function dismiss(){
+
+    
+    console.log("dn");
+    let name = document.getElementById("user")
+    if (name.value != "") {
+        signbtn.style.width = getComputedStyle(document.querySelector("#prototype")).width
+        let myname = name.value
+        ppup.style.display="none";
+        signbtn.innerText = "Welcome! "+myname
+        signbtn.setAttribute("onclick","")
+        signbtn.classList.add("dont")
+    }else{
+        alert("username is empty!")
+    }
+
 }
